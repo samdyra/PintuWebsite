4062b7c01dbc8eb2fb6aec702d27f1818ef7267e
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-03-10 01:41:44 +0700

    Develop Table Mobile Version

--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 const Table:React.FC = () => (
   <div className="">
-    <header className='flex font-bold text-xs border-y-2 py-3 px-8'>
+    <header className='flex font-bold text-xs border-y-2 py-5 px-8'>
       <h3>CRYPTO</h3>
     </header>
+    <table className='w-full'>
+      <tbody className='flex border-b-2 py-5 w-full justify-between'>
+        <section className='flex pl-2 w-36 h-12 justify-around items-center'>
+          <img className='h-8' src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png' alt='image-coin'></img>
+          <div className='flex flex-col items-start'>
+            <h3 className='font-bold'>Bitcoin</h3>
+            <p className='text-gray-500 text-sm'>BTC</p>
+          </div>
+        </section>
+        <section className='flex flex-col pr-4 w-52 items-end'>
+          <h4 className='font-bold'>Rp500.000.000</h4>
+          <h3 className='font-medium text-green-600'>2%</h3>
+        </section>
+      </tbody>
+    </table>
   </div>
-
 )
 
 export default Table
